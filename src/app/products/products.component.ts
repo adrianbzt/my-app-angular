5f3d0eb4baf2b6948167721f16e97fbc7a39752a
Adrian Buzatu <adrian.buzatu@gameloft.com>
2019-07-29 12:54:17 +0300

    work

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products = [
+    'pere',
+    'mere',
+    'ana',
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
